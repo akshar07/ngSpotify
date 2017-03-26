5104bf9706e7163191c011e59cb1d9081a711abb
akshar07 <takleakshar@gmail.com>
2017-03-26 13:21:21 -0700

    final version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,13 @@ import { ROUTES } from './app.routes';
 import { SearchResultsComponent } from './search-results/search-results.component';
 import { SpotifyService } from './service/spotify.service';
 import { Http,HttpModule } from '@angular/http';
-import { OrderByPipe } from './orderby';
 import { AboutComponent } from './about.component';
 import { ArtistComponent } from './artist/artist.component';
 
 @NgModule({
   imports:      [ BrowserModule,FormsModule,HttpModule,
                 RouterModule.forRoot(ROUTES,{useHash:true}) ],
-  declarations: [ AppComponent,SearchResultsComponent,OrderByPipe,AboutComponent,ArtistComponent ],
+  declarations: [ AppComponent,SearchResultsComponent,AboutComponent,ArtistComponent ],
   bootstrap:    [ AppComponent ],
   providers:[SpotifyService]
 })
